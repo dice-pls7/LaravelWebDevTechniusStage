0bf4a4bb27cc0c3c4fadd93b3c23d621b5776106
vince <S1181118@student.windesheim.nl>
2024-05-09 10:10:41 +0200

    !!! Screenshot test code. Screenshot + mail openen. Ook JS files toegevoegd en JScanvas

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="{{ asset('css/Header.css') }}">
     <link rel="stylesheet" href="{{ asset('css/Footer.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
     @include('Header') 
 </head>
 <body>
@@ -17,7 +18,7 @@
 
 <div class="Gegevenstabel {{$kandidaat->functie}}"> <!-- Hier wordt de functie van de kandidaat meegegeven als class voor de kleur-->
 
-    <div class="Gegevens">
+    <div id="capture" class="Gegevens">
         @if(Route::has('login'))
             @auth
                 <div class="DeleteKnop">
@@ -25,6 +26,49 @@
                 </div>
             @endauth
         @endif
+
+
+
+         <!-- Voeg een knop toe om het screenshot te maken -->
+    <button id="screenshotBtn">Maak Screenshot</button>
+    <!-- Voeg een knop toe om de screenshot naar Outlook te verzenden -->
+    <button id="sendEmailBtn">Verstuur Screenshot via Outlook</button>
+
+    <script>
+        document.getElementById("screenshotBtn").addEventListener("click", function() {
+            // Selecteer het element dat je wilt vastleggen
+            var element = document.getElementById("capture");
+
+            // Gebruik HTML2Canvas om het element te renderen naar een canvas
+            html2canvas(element, {
+                onrendered: function(canvas) {
+                    document.body.appendChild(canvas);
+                    // Converteer het canvas naar een Blob-object
+                    canvas.toBlob(function(blob) {
+                        // Sla de Blob op als een globale variabele zodat we er later toegang toe hebben
+                        window.screenshotBlob = blob;
+                    });
+                }
+            });
+        });
+
+        document.getElementById("sendEmailBtn").addEventListener("click", function() {
+            // Controleer of er een screenshot is gemaakt
+            if (window.screenshotBlob) {
+                // Maak een URL van de Blob
+                var blobUrl = URL.createObjectURL(window.screenshotBlob);
+                // Genereer de mailto-link met de Blob als bijlage
+                var mailtoLink = "mailto:?subject=Screenshot&body=Hier komt nog de template van de mail &attachment=" + encodeURIComponent(URL.createObjectURL(window.screenshotBlob));
+                // Open de mailto-link
+                window.location.href = mailtoLink;
+            } else {
+                alert("Maak eerst een screenshot voordat je de e-mail verzendt.");
+            }
+        });
+    </script>
+
+
+
             <h2>{{ $kandidaat->voornaam }} {{ $kandidaat->tussenvoegsel }} {{ $kandidaat->achternaam }}</h2>
             <p><span>Geboortedatum: </span>{{ $kandidaat->geboortedatum }}</p>
             <p><span>Functie: </span>{{ $kandidaat->functie }}</p>
