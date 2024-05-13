53e269b40ba4fbb32f541cc1c80796ab587f70b4
vince <S1181118@student.windesheim.nl>
2024-05-13 09:29:34 +0200

    commentaar toegevoegd

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -19,41 +19,41 @@ class FilterController extends Controller
     {
         return view('overzicht');
     }
-    public function filterResults(Request $request)
+    public function filterResults(Request $request) 
     {
     $kandidaten = $this->getFilteredKandidaten($request);
     return view('overzicht', ['kandidaten' => $kandidaten]);
     }
-    public function getFilteredKandidaten(Request $request)
+    public function getFilteredKandidaten(Request $request) 
     {
-    $kandidaten = $this->controller->getAllKandidaten();
+    $kandidaten = $this->controller->getAllKandidaten(); 
 
-    $filteredKandidaten = [];
+    $filteredKandidaten = []; // Create an empty array to store the filtered kandidaten
 
-    foreach ($kandidaten as $kandidaat) {
-        if ($request->filled('functie') && $kandidaat->functie != $request->input('functie')) {
+    foreach ($kandidaten as $kandidaat) { // Loop through all kandidaten
+        if ($request->filled('functie') && $kandidaat->functie != $request->input('functie')) { // Check if the functie is set and if it matches the kandidaat's functie
+            continue; // Skip the current iteration
+        }
+        if ($request->filled('beschikbaarheid') && $kandidaat->beschikbaar != $request->input('beschikbaarheid')) { // Check if the beschikbaarheid is set and if it matches the kandidaat's beschikbaarheid
             continue;
         }
-        if ($request->filled('beschikbaarheid') && $kandidaat->beschikbaar != $request->input('beschikbaarheid')) {
-            continue;
-        }
-        if ($request->input('werkervaring')) {
-            $werkervaring = $request->input('werkervaring');
-            if ($werkervaring === '20+') {
-                if ($kandidaat->werkervaring <= 20) {
+        if ($request->input('werkervaring')) { // Check if the werkervaring is set
+            $werkervaring = $request->input('werkervaring'); // Get the werkervaring value
+            if ($werkervaring === '20+') { // Check if the werkervaring is 20+
+                if ($kandidaat->werkervaring <= 20) { // Check if the kandidaat's werkervaring is less than or equal to 20
                     continue;
                 }
-            } else {
-                $werkervaringRange = explode('-', $werkervaring);
-                $minWerkervaring = intval($werkervaringRange[0]);
-                $maxWerkervaring = intval($werkervaringRange[1]);
+            } else { // If the werkervaring is not 20+
+                $werkervaringRange = explode('-', $werkervaring); // Split the werkervaring value into an array
+                $minWerkervaring = intval($werkervaringRange[0]); // Get the minimum werkervaring
+                $maxWerkervaring = intval($werkervaringRange[1]); // Get the maximum werkervaring
 
-                if ($kandidaat->werkervaring < $minWerkervaring || $kandidaat->werkervaring > $maxWerkervaring) {
+                if ($kandidaat->werkervaring < $minWerkervaring || $kandidaat->werkervaring > $maxWerkervaring) {  // Check if the kandidaat's werkervaring is less than the minimum werkervaring or greater than the maximum werkervaring
                     continue;
                 }
             }
         }
-        $filteredKandidaten[] = $kandidaat;
+        $filteredKandidaten[] = $kandidaat; // Add the kandidaat to the filteredKandidaten array
     }
     return $filteredKandidaten;
     }
