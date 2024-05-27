36cb9c28dad28b334513213d95d8af44441cac59
Tim Nijveld <s1185956@student.windesheim.nl>
2024-05-27 15:26:49 +0200

    Logo als achtergrond. footer.html naar footer.blade.php

--- resources/views/overzicht.blade.php
+++ resources/views/overzicht.blade.php
@@ -9,77 +9,78 @@
     @include('Header')
 </head>
 <body>
-    <div class="Filter">
-        <button id="FilterButton" onclick="toggleFilters()">Filteren op</button>
-        <button id="WisFilter" onclick="window.location.href='{{ route('overzicht') }}'">Wis filters</button>
-        <!-- Filter options -->
-        <div class="filters" id="Filters">
-            <form action="{{ route('overzicht') }}" method="POST">
-                @csrf
-                <button type="reset" onclick="toggleFilters()">X</button>
+    <div class="content-container">
+        <div class="Filter">
+            <button id="FilterButton" onclick="toggleFilters()">Filteren op</button>
+            <button id="WisFilter" onclick="window.location.href='{{ route('overzicht') }}'">Wis filters</button>
+            <!-- Filter options -->
+            <div class="filters" id="Filters">
+                <form action="{{ route('overzicht') }}" method="POST">
+                    @csrf
+                    <button type="reset" onclick="toggleFilters()">X</button>
 
-                <label for="functie">Functie</label>
-                <select name="functie" id="functie">
-                    <option value="">Kies een optie</option>
-                    <option value="Loodgieter">Loodgieter</option>
-                    <option value="Elektromonteur">Elektromonteur</option>
-                </select>
+                    <label for="functie">Functie</label>
+                    <select name="functie" id="functie">
+                        <option value="">Kies een optie</option>
+                        <option value="Loodgieter">Loodgieter</option>
+                        <option value="Elektromonteur">Elektromonteur</option>
+                    </select>
 
-                <label for="beschikbaar">Beschikbaar</label>
-                <select name="beschikbaarheid" id="beschikbaarheid">
-                    <option value="">Kies een optie</option>
-                    <option value="1">Ja</option>
-                    <option value="0">Nee</option>
-                </select>
+                    <label for="beschikbaar">Beschikbaar</label>
+                    <select name="beschikbaarheid" id="beschikbaarheid">
+                        <option value="">Kies een optie</option>
+                        <option value="1">Ja</option>
+                        <option value="0">Nee</option>
+                    </select>
 
-                <button type="reset">Reset filter opties</button>
-                <button type="submit">Filteren</button>
-            </form>
+                    <button type="reset">Reset filter opties</button>
+                    <button type="submit">Filteren</button>
+                </form>
+            </div>
         </div>
-    </div>
 
-<div class="candidates">
-    @foreach ($kandidaten as $kandidaat)
+        <div class="candidates">
+            @foreach ($kandidaten as $kandidaat)
+                @php
+                    $class = '';
+                    if ($kandidaat->Functie == 'Loodgieter') {
+                        $class = 'Loodgieter';
+                    } elseif ($kandidaat->Functie == 'Elektromonteur') {
+                        $class = 'Elektromonteur';
+                    }
+                @endphp
 
-            @php
-                $class = '';
-                if ($kandidaat->Functie == 'Loodgieter') {
-                    $class = 'Loodgieter';
-                } elseif ($kandidaat->Functie == 'Elektromonteur') {
-                    $class = 'Elektromonteur';
-                }
-            @endphp
-
-        <a class="candidate {{ $class }}" href="{{ url('details/' . $kandidaat->Id) }}" class="details">
-        @if(Route::has('login'))
+                <a class="candidate {{ $class }}" href="{{ url('details/' . $kandidaat->Id) }}" class="details">
+                @if(Route::has('login'))
                     @auth
-            @if($kandidaat->pinned == 1)
-            <button title="Kandidaat is gepind" type="button" id="PinKnop" onclick="" ><i class="fas fa-thumbtack"></i></button>
-            @endif
-            @endauth
-        @endif
-            <h2>{{ $kandidaat->Voornaam }} {{ substr($kandidaat->Achternaam, 0, 1) }}</h2>
+                        @if($kandidaat->pinned == 1)
+                            <button title="Kandidaat is gepind" type="button" id="PinKnop" onclick="" ><i class="fas fa-thumbtack"></i></button>
+                        @endif
+                    @endauth
+                @endif
+                    <h2>{{ $kandidaat->Voornaam }} {{ substr($kandidaat->Achternaam, 0, 1) }}</h2>
 
-            <!-- Omzetten van datum naar Nederlandse notatie -->
-            <p>Geboortedatum: {{ date('d-m-Y', strtotime($kandidaat->Geboortedatum)) }}</p>
-            <p>Functie: {{ $kandidaat->Functie }}</p>
-            <p>Werkervaring: {{ $kandidaat->Werkervaring }} jaar</p>
-        </a>
-    @endforeach
-</div>
-    @if($kandidaten instanceof \Illuminate\Pagination\AbstractPaginator)
-        <div class="mt-3">
-            {{ $kandidaten->links() }}
+                    <!-- Omzetten van datum naar Nederlandse notatie -->
+                    <p>Geboortedatum: {{ date('d-m-Y', strtotime($kandidaat->Geboortedatum)) }}</p>
+                    <p>Functie: {{ $kandidaat->Functie }}</p>
+                    <p>Werkervaring: {{ $kandidaat->Werkervaring }} jaar</p>
+                </a>
+            @endforeach
         </div>
-    @endif
+        @if($kandidaten instanceof \Illuminate\Pagination\AbstractPaginator)
+            <div class="mt-3">
+                {{ $kandidaten->links() }}
+            </div>
+        @endif
+    </div>
+    <footer>
+        @include('Footer')
+    </footer>
+    <script>
+        function toggleFilters() {
+            var filters = document.getElementById("Filters");
+            filters.style.display = (filters.style.display === "block") ? "none" : "block";
+        }
+    </script>
 </body>
-<script>
-    function toggleFilters() {
-        var filters = document.getElementById("Filters");
-        filters.style.display = (filters.style.display === "block") ? "none" : "block";
-    }
-</script>
-<footer>
-    @include('footer')
-</footer>
 </html>
