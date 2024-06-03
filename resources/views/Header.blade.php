3ac950e004d5dfa38db39ebc5659405fcc5b94dc
TimNijveld <s1185956@student.windesheim.nl>
2024-06-03 12:01:12 +0200

    Aanvraag knop

--- resources/views/Header.blade.php
+++ resources/views/Header.blade.php
@@ -6,6 +6,24 @@
     <link rel="stylesheet" href="{{ asset('css/Header.css') }}">
 </head>
 <body>
+        <?php
+                // Define variables for email content
+                $to = "hello@technius.nl";
+                $subject = "Aanvraag";
+                $body = "Beste Team Technius," . "\n\n";
+                $body .= "Graag zou ik een aanvraag willen indienen voor het volgende:" . "\n\n";
+                $body .= "Project:" . "\n";
+                $body .= "Startdatum:" . "\n";
+                $body .= "Type monteur:" . "\n";
+                $body .= "\n";
+                $body .= "Ik ben te bereiken op: 06-";
+                $body .= "\n\n";
+                $body .= "Met vriendelijke groet," . "\n\n";
+                $body .= "[Uw Naam]";
+
+                // Encode the email body for URL
+                $body_encoded = rawurlencode($body);
+            ?>
     <nav>
         <div class="logo">
             <a href="{{ url('/overzicht') }}">
@@ -14,6 +32,7 @@
         <ul class="menu-items">
             @if(Route::has('login'))
                 @auth
+                <li class="hideOnMobile"><a href="mailto:<?php echo $to; ?>?subject=<?php echo $subject; ?>&body=<?php echo $body_encoded; ?>" target="_blank" id="Aanvraag">Aanvraag doen</a></li>
                     <li class="hideOnMobile"><a href="{{ url('/kandidaattoevoegen') }}" id="toevoegenKandidaat">Kandidaat Toevoegen</a></li>
                     <li class="hideOnMobile"><a href="{{ route('profile.edit') }}" id="Profile">Profiel</a></li>
                     <li class="hideOnMobile">
@@ -22,11 +41,10 @@
                             <button type="submit" id="Logout">Log uit</button>
                         </form>
                     </li>
-                @else
+                    @else
+            <li class="hideOnMobile"><a href="mailto:<?php echo $to; ?>?subject=<?php echo $subject; ?>&body=<?php echo $body_encoded; ?>" target="_blank" id="Aanvraag">Aanvraag doen</a></li>
                     <li class="hideOnMobile"><a href="#footer" id="Contact">Neem contact op</a></li>
                     <li class="hideOnMobile"><a href="{{ route('login') }}" id="Login">Inloggen</a></li>
-                @endauth
-            @endif
             <li class="menu-button" onclick="toggleMobileMenu()">
                 <a href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed">
@@ -34,6 +52,8 @@
                     </svg>
                 </a>
             </li>
+            @endauth
+            @endif
         </ul>
         <ul class="sidebar">
             <li>
@@ -45,6 +65,7 @@
             </li>
             @if(Route::has('login'))
                 @auth
+                    <li class="hideOnMobile"><a href="mailto:<?php echo $to; ?>?subject=<?php echo $subject; ?>&body=<?php echo $body_encoded; ?>" target="_blank" id="Aanvraag">Aanvraag doen</a></li>
                     <li><a href="{{ url('/kandidaattoevoegen') }}">Kandidaat Toevoegen</a></li>
                     <li><a href="{{ route('profile.edit') }}">Profiel</a></li>
                     <li>
@@ -53,11 +74,13 @@
                             <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">Log uit</a>
                         </form>
                     </li>
-                @else
+                    @else
+                    <li><a href="mailto:<?php echo $to; ?>?subject=<?php echo $subject; ?>&body=<?php echo $body_encoded; ?>" target="_blank">Aanvraag doen</a></li>
                     <li><a href="#footer">Neem contact op</a></li>
                     <li><a href="{{ route('login') }}">Inloggen</a></li>
-                @endauth
+            @endauth
             @endif
+
         </ul>
     </nav>
     <script>
