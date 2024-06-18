d40572ef9c55e996bab9b0dab050d09ff2797adf
vince <S1181118@student.windesheim.nl>
2024-06-18 10:28:56 +0200

    Test

--- e2e/example.spec.cjs
+++ e2e/example.spec.cjs
@@ -18,10 +18,61 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
+test('Kandidaat aanmaken', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/overzicht');
+  await page.getByRole('link', { name: 'Inloggen' }).click();
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('Test@mail.com');
+  await page.getByLabel('Email').press('Tab');
+  await page.getByLabel('Password').fill('P@ssw0rd');
+  await page.getByLabel('Password').press('Enter');
+  await page.getByRole('link', { name: 'Kandidaat Toevoegen' }).click();
+  await page.getByLabel('Voornaam *').click();
+  await page.getByLabel('Voornaam *').fill('Test123');
+  await page.getByLabel('Voornaam *').press('Tab');
+  await page.getByLabel('Tussenvoegsel').press('Tab');
+  await page.getByLabel('Achternaam *').fill('Test');
+  await page.getByLabel('Achternaam *').press('Tab');
+  await page.getByLabel('Geboortedatum *').fill('1999-01-01');
+  await page.getByLabel('Geboortedatum *').press('Tab');
+  await page.getByLabel('Beschikbaar vanaf: *').fill('1999-01-01');
+  await page.getByLabel('Beschikbaar vanaf: *').press('Tab');
+  await page.getByLabel('Functie *').selectOption('Loodgieter');
+  await page.getByLabel('Beschikbaar *').selectOption('1');
+  await page.getByLabel('Locatie *').click();
+  await page.getByLabel('Locatie *').fill('Zwolle');
+  await page.getByLabel('Taal *').click();
+  await page.getByLabel('Taal *').fill('Spreekt code');
+  await page.getByLabel('Taal *').press('Tab');
+  await page.getByLabel('Werkervaring *').fill('Ja');
+  await page.getByLabel('Werkervaring *').click();
+  await page.getByLabel('Werkervaring *').fill('1');
+  await page.getByLabel('Beschrijving kandidaat').click();
+  await page.getByLabel('Beschrijving kandidaat').fill('Mr code');
+  await page.getByRole('button', { name: 'Toevoegen' }).click();
 
+});
+
+test('Inloggen', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/overzicht');
+  await page.getByRole('link', { name: 'Inloggen' }).click();
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('aw');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('aw');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('Test@mail.com');
+  await page.getByLabel('Email').press('Tab');
+  await page.getByLabel('Password').fill('P@ssw0rd');
+  await page.getByLabel('Password').press('Enter');
+  
+});
 
 test('Gebruiker wijzigen', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8000/');
+  await page.goto('http://127.0.0.1:8000/login');
   await page.getByLabel('Email').click();
   await page.getByLabel('Email').fill('test@mi');
   await page.locator('div').filter({ hasText: 'Heb je een account? Log dan' }).first().click();
@@ -70,5 +121,18 @@ test('Gebruiker wijzigen', async ({ page }) => {
   await page.getByLabel('Certificaten').fill('Heftruckcertificaat12');
   await page.getByLabel('Beschrijving kandidaat').click();
   await page.getByLabel('Beschrijving kandidaat').fill('De goated');
-  await page.getByRole('button', { name: 'Wijzingen opslaan' }).click();
+  
 }); 
+
+
+test('Detailpagina openen en interesse mail sturen', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/overzicht');
+  await page.getByRole('link', { name: 'Pietje1234 V Elektromonteur' }).click();
+  await page.getByRole('button', { name: 'Interesse? Mail ons!' }).click();
+  await page.getByRole('link', { name: 'Terug naar overzicht' }).click();
+});
+
+test('Aanvraag doen', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/overzicht');
+  await page.getByRole('link', { name: 'Aanvraag doen' }).click();
+});
